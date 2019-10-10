b58098f529a02804efa3b13c76c7ef4543cc47b8
Brent Woo <brewoo@amazon.com>
2019-10-10 16:54:55 -0400

    change watcher

--- watch_lilypond.sh
+++ watch_lilypond.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-fswatch -0 -event ./corn.ly | xargs -0 -n 1 -I {} lilypond corn.ly
+echo "====================================="
+echo "*** Ok! Watching" $1 "****"
+echo "====================================="
+fswatch -0 -event $1 | xargs -0 -n 1 -I {} lilypond $1
